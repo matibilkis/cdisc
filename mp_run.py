e3891f513cbf5277d475a7ab3be441ac81234f8b
matibilkis <matiasbilkis@gmail.com>
2022-06-03 00:38:41 +0200

    clean up

---
+++ mp_run.py
@@ -0,0 +1,19 @@
+import os
+import multiprocessing as mp
+from numerics.utilities.misc import *
+
+
+cores = mp.cpu_count()
+
+mode="frequencies"
+dt = 1e-4
+total_time = 200.
+ppp=5*1e3
+def int_seed(seed):
+    for k in range(10):
+        os.system("python3 numerics/integration/integrate.py --itraj {} --mode {} --dt {} --total_time {} --ppp {}".format(seed+k, mode, dt, total_time, ppp))
+        os.system("python3 numerics/integration/integrate.py --itraj {} --flip_params 1 --mode {} --dt {} --total_time {} --ppp {}".format(seed+k, mode, dt, total_time, ppp))
+        print(f"{k}, {seed}, done")
+
+with mp.Pool(cores-10) as p:
+    p.map(int_seed, range(1000,5000, 10))
---
+++ numerics/utilities/misc.py
@@ -0,0 +1,81 @@
+import numpy as np
+import ast
+import os
+import getpass
+
+def give_model():
+    return "optical"
+
+def get_def_path():
+    """
+    mode is changed from branch to branch (from model to model)
+    """
+    model =give_model()
+    user = getpass.getuser()
+    if user == "cooper-cooper":
+        defpath = '../quantera/trajectories/'
+    elif (user =="matias") or (user == "mati"):# or (user=="giq"):
+        defpath = '../quantera/trajectories/'
+    elif (user=="giq"):
+        defpath = "/media/giq/Nuevo vol/quantera/trajectories/"
+    else:
+        defpath = "/data/uab-giq/scratch/matias/quantera/trajectories/"
+    if mode[-1] != "/":
+        mode+="/"
+    defpath+=mode
+    return defpath
+
+
+def def_params(flip =0):
+    model = give_model()
+    if model == "mechanical":
+        gamma0 = gamma1 = 1000
+        eta0 = eta1 = 1
+        kappa0 = kappa1 = 1e6
+        n0 = n1 = 1
+        omega0, omega1 = 1e4, 1.05e4
+
+        h0 = [gamma0, omega0, n0, eta0, kappa0]
+        h1 = [gamma1, omega1, n1, eta1, kappa1]
+        if flip == 0:
+            p = [h1, h0]
+        else:
+            p = [h0, h1]
+
+    elif model == "optical":  #genoni's paper
+        kappa0 = kappa1 = 1.
+        xi0 = xi1 = 0.1*kappa1
+        eta0 = eta1 = .9
+        omega0, omega1 = 0.1*kappa1, 0.2*kappa1
+
+        h0 = [kappa0, eta0, omega0, xi0]
+        h1 = [kappa1, eta1, omega1, xi1]
+
+        if flip == 0:
+            p = [h1, h0]
+        else:
+            p = [h0, h1]
+    return p, str(p)+"/"
+
+
+def get_path_config(exp_path="", itraj=1, total_time=1, dt=.1):
+    pp = get_def_path()+ exp_path +"{}itraj/T_{}_dt_{}/".format(itraj, total_time, dt)
+    return pp
+
+
+def load_data(exp_path="", itraj=1, total_time=1, dt=0.1, what="logliks.npy"):
+    path = get_path_config(total_time = total_time, dt= dt, itraj=itraj, exp_path=exp_path)
+    logliks = np.load(path+what,allow_pickle=True,fix_imports=True,encoding='latin1') ### this is \textbf{q}(t)
+    return logliks
+
+
+def load_liks(itraj=1, dt=1e-1, total_time=1):
+    params, exp_path = def_params(flip=0)
+    logliks =load_data_discrimination_liks(itraj=itraj, total_time = total_time, dt=dt, exp_path = exp_path)
+    l1  = logliks[:,0] - logliks[:,1]
+
+    params, exp_path = def_params(flip=1)
+    logliks =load_data_discrimination_liks(itraj=itraj, total_time = total_time, dt=dt, exp_path = exp_path)
+    l0  = logliks[:,0] - logliks[:,1]
+
+    return l0, l1#, tims
