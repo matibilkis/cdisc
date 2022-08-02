1525ab4452630580dc408843663b980948f127f6
mati <matiasbilkis@gmail.com>
2022-08-02 17:45:08 -0300

    integration step corrected

--- mp_run.py
+++ mp_run.py
@@ -5,15 +5,15 @@ from datetime import datetime
 
 cores = mp.cpu_count()
 
+pdts = [1, 10, 100, 1000]
 def int_seed(seed):
-    for k in range(Nstep):
+    for pdt in pdts:
         s1 = datetime.now()
-        os.system("python3 numerics/integration/integrate.py --itraj {}".format(seed+k))
-        os.system("python3 numerics/integration/integrate.py --itraj {} --flip_params 1".format(seed+k))
-        print(f"{k}, {seed}, done")
+        os.system("python3 numerics/integration/integrate.py --itraj {} --pdt {} --total_time 1.".format(seed, pdt))
+        #os.system("python3 numerics/integration/integrate.py --itraj {} --flip_params 1".format(seed+k))
+        print(f"{seed} done")
+        #print(f" {seed}, done")
 
-cores = 20
-Nstep = 20
 int_seed(1)
 with mp.Pool(cores-1) as p:
-    p.map(int_seed, range(5000,10000, Nstep))
+    p.map(int_seed, range(1,100))
