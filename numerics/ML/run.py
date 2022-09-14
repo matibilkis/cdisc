4cd8c99d8dfd551945ed6c5743a6d5ad34ece8d3
mati <matiasbilkis@gmail.com>
2022-09-14 23:34:13 +0200

    a

--- numerics/ML/run.py
+++ numerics/ML/run.py
@@ -72,7 +72,6 @@ params = [gamma, omega, n, eta, kappa]
 exp_path = str(params)+"/"
 states = load_data(exp_path=exp_path,total_time=total_time, dt=dt,what="states.npy", itraj=itraj)
 signals = load_data(exp_path=exp_path,total_time=total_time, dt=dt,what="signals.npy", itraj=itraj)
-tfsignals = misc_ML.pre_process_data_for_ML(times, signals[:-1])
 
 gamma, omega, n, eta, kappa = np.array(params).astype("float32")
 A = np.array([[-gamma/2, omega],[-omega, -gamma/2]]).astype("float32")
@@ -80,34 +79,32 @@ C = np.sqrt(4*eta*kappa)*np.array([[1.,0.],[0., 0.]]).astype("float32")
 D = np.diag([gamma*(n+0.5) + kappa]*2).astype("float32")
 cov_st = solve_continuous_are( A.T, C.T, D, np.eye(2))
 
+timms = np.linspace(100, len(times)-1,20).astype("int")
+for train_id, tt in enumerate(timms):
+    tfsignals = misc_ML.pre_process_data_for_ML(times[:tt], signals[:tt-1])
 
-save_dir = misc_ML.get_training_save_dir(exp_path, total_time, dt, itraj,train_id)
-os.makedirs(save_dir, exist_ok=True)
+    save_dir = misc_ML.get_training_save_dir(exp_path, total_time, dt, itraj,train_id)
+    os.makedirs(save_dir, exist_ok=True)
 
-initial_parameters = np.array([10*np.random.uniform()*omega]).astype("float32")
-true_parameters = np.array([omega]).astype("float32")
+    initial_parameters = np.array([10*np.random.uniform()*omega]).astype("float32")
+    true_parameters = np.array([omega]).astype("float32")
 
+    epochs = 200
+    learning_rate = 10
 
-batch_size = 100
-epochs = 200
-learning_rate = 10
-
-with open(save_dir+"training_details.txt", 'w') as f:
-    f.write("BS: {}\nepochs: {}\n learning_rate: {}\n".format(batch_size, epochs, learning_rate))
-f.close()
-
-
-model = model_ML.Model(params=params, dt=dt, initial_parameters=initial_parameters,
-              true_parameters=true_parameters, initial_states = np.zeros((1,5)).astype(np.float32),
-              cov_in=cov_st, batch_size=tuple([None,None,3]),
-              save_dir = save_dir)
-model.recurrent_layer.build(tf.TensorShape([1, None, 3])) #None frees the batch_size
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate))
-
-history = model.craft_fit(tfsignals, batch_size=100, epochs=epochs, early_stopping=1e-14)
+    with open(save_dir+"training_details.txt", 'w') as f:
+        f.write("BS: {}\nepochs: {}\n learning_rate: {}\n".format(batch_size, epochs, learning_rate))
+    f.close()
 
 
 
+    model = model_ML.Model(params=params, dt=dt, initial_parameters=initial_parameters,
+                  true_parameters=true_parameters, initial_states = np.zeros((1,5)).astype(np.float32),
+                  cov_in=cov_st, batch_size=tuple([None,None,3]),
+                  save_dir = save_dir)
+    model.recurrent_layer.build(tf.TensorShape([1, None, 3])) #None frees the batch_size
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate))
+    history = model.craft_fit(tfsignals[:,:tt,:], batch_size=min(times[tt],100), epochs=epochs, early_stopping=1e-14)
 
 
 
