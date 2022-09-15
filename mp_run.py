93c410bec626b2be58d55dabd87666fc22c48801
matibilkis <matiasbilkis@gmail.com>
2022-09-15 04:39:37 +0200

    some typos

--- mp_run.py
+++ mp_run.py
@@ -4,11 +4,11 @@ from numerics.utilities.misc import *
 from datetime import datetime
 import argparse
 
-cores = 6
+cores = 18
 
 trajs = list(range(1,int(1e4)))
 def simu(itraj):
     os.system("python3 numerics/integration/integrate.py --itraj {}".format(itraj))
-
+    print(itraj)
 with mp.Pool(cores) as p:
     p.map(simu, trajs)
--- numerics/ML/mp_train.py
+++ numerics/ML/mp_train.py
@@ -1,14 +1,17 @@
 import os
+import sys
+sys.path.insert(0, os.getcwd())
 import multiprocessing as mp
 from numerics.utilities.misc import *
 from datetime import datetime
 import argparse
 
-cores = 18
+
+cores = 16
 
 trajs = list(range(1,int(1e4)))
 def simu(itraj):
-    os.system("python3 ML/run.py --itraj {}".format(itraj))
-
+    os.system("python3 numerics/ML/run.py --itraj {}".format(itraj))
+    print(itraj)
 with mp.Pool(cores) as p:
     p.map(simu, trajs)
