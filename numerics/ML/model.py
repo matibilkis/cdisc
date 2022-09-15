depending on the model, the derivative was wrong! I was missing a term. I've added in the Fhidden

numerics/ML/model.py
@@ -47,9 +47,10 @@ class GRCell(tf.keras.layers.Layer):
         inns = tf.squeeze(inputs)
         time, dy = inns[0], inns[1:][tf.newaxis]
 
+        print("WARGNING")
         sts = states[0][:,:2]
         self.ss.append(sts)
-        cov = tf.convert_to_tensor(np.array([states[1]#self.cov_in
+        #cov = tf.convert_to_tensor(np.array([states[1]#self.cov_in
 
 
         XiCov =tf.einsum('bij,jk->bik',cov,self.C_matrix.T)

numerics/ML/test.py
@@ -72,6 +72,20 @@ A = np.array([[-gamma/2, omega],[-omega, -gamma/2]]).astype("float32")
 C = np.sqrt(4*eta*kappa)*np.array([[1.,0.],[0., 0.]]).astype("float32")
 D = np.diag([gamma*(n+0.5) + kappa]*2).astype("float32")
 cov_st = solve_continuous_are( A.T, C.T, D, np.eye(2))
+D_th = np.eye(2)*0. ## we estimate \omega
+A_th = np.array([[0.,1.],[-1.,0.]])
+cov_st_th = solve_continuous_are( (A-np.dot(cov_st,np.dot(C.T,C))).T, np.eye(2)*0., D_th + np.dot(A_th, cov_st) + np.dot(cov_st, A_th.T), np.eye(2))
+
+from scipy.linalg import block_diag
+
+XiCov  = np.dot(cov_st, C.T) #I take G=0.
+XiCov_th  = np.dot(cov_st_th, C.T) #I take G = 0.
+
+cth = cov_st_th
+
+cth.dot((A - cov_st.dot(C.dot(C))).T) + (A - cov_st.dot(C.dot(C))).dot(cth) + cov_st.dot(A_th.T) + A_th.dot(cov_st)
+
+
 
 
 states = load_data(exp_path=exp_path,total_time=total_time, dt=dt,what="states.npy", itraj=itraj)
@@ -122,6 +136,7 @@ class GRCell(tf.keras.layers.Layer):
         sts = states[0][:,:2]
         cc = states[0][0,2:]
         cov=tf.convert_to_tensor(np.array([[cc[0], cc[1]],[cc[1], cc[2]]]))[tf.newaxis]
+        print(cov)
         self.ss.append(sts)
         #cov = self.cov_in
 
@@ -204,6 +219,21 @@ nsignals = np.squeeze(tfsignals)
 plt.plot(times[:tt], states[:tt,0])
 plt.plot(nsignals[:,0],np.squeeze(preds)[:,0])
 
+
+
+
+
+model.reset_states()
+model.layers[0].return_sequences = True
+preds = model(tfsignals[:,:k,:])
+
+with tf.GradientTape(persistent=True) as tape:
+    tape.watch(model.trainable_variables)
+    preds = model(tfsignals[:,:k,:])
+
+tape.gradient(preds,model.trainable_variables)
+
+
 from tqdm import tqdm
 grads = []
 for k in tqdm(range(1,100,1)):
@@ -220,8 +250,8 @@ for k in tqdm(range(1,100,1)):
 grads_ =np.stack(grads)
 
 tt=99
-plt.plot(times[:tt],grads_[:,0])
-plt.plot(times[:tt],states_th[:tt,0])
+plt.plot(times[:tt],grads_[:tt,0])
+#plt.plot(times[:tt],states_th[:tt,0])
 
 
 

numerics/integration/integrate.py
@@ -39,7 +39,7 @@ def Fhidden(s, t, dt):
     x = s[:2]
     x_th = s[2:4]
     x_dot = np.dot(A,x)
-    x_th_dot = np.dot(A, x_th) + np.dot(A_th,x)
+    x_th_dot = np.dot(A - XiCovC, x_th) + np.dot(A_th,x)
     return np.array(list(x_dot) + list(x_th_dot))
 
 @jit(nopython=True)
@@ -50,7 +50,7 @@ def integrate(params, total_time=1, dt=1e-1, itraj=1, exp_path="",**kwargs):
     """
     h1 is the hypothesis i use to get the data. (with al the coefficients gamma1...)
     """
-    global proj_C, A, A_th, XiCov_th, XiCov, C, dW, model, big_XiCov
+    global proj_C, A, A_th, XiCov_th, XiCov, C, dW, model, big_XiCov, XiCovC
     model = give_model()
     pdt = kwargs.get("pdt",1)
     dt *=pdt #this is to check accuracy of integration
@@ -80,6 +80,7 @@ def integrate(params, total_time=1, dt=1e-1, itraj=1, exp_path="",**kwargs):
     cov_st_th = solve_continuous_are( (A-np.dot(cov_st,np.dot(C.T,C))).T, np.eye(2)*0., D_th + np.dot(A_th, cov_st) + np.dot(cov_st, A_th.T), np.eye(2))
 
     XiCov  = np.dot(cov_st, C.T) #I take G=0.
+    XiCovC = np.dot(XiCov, C)
     XiCov_th  = np.dot(cov_st_th, C.T) #I take G = 0.
 
     big_XiCov = block_diag(XiCov, XiCov_th)