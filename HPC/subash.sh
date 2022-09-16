ca981cbe5acaec400bcaa3aa0d275ec8058f660a
mati <matiasbilkis@gmail.com>
2022-09-16 10:53:13 +0200

    changed lr

--- HPC/subash.sh
+++ HPC/subash.sh
@@ -3,11 +3,9 @@ itraj=$1
 cd ~/estimation
 . ~/qenv_bilkis/bin/activate
 START=$(date +%s.%N)
-python3 numerics/integration/integrate.py --itraj $itraj
-END=$(date +%s.%N)
-DIFF=$(echo "$END - $START" | bc)
-echo $DIFF
-python3 numerics/ML/run.py --itraj $itraj
+python3 ~/estimation/integration/integrate.py --itraj $itraj
+echo "integration done"
+python3 ~/estimation/numerics/ML/run.py --itraj $itraj
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 echo $DIFF
